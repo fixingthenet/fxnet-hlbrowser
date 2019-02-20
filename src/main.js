92ad7064cc3cfa307b71aaeb7ff343f72b74d0fa
Peter Schrammel <peter.schrammel@preisanalytics.de>
2019-02-20 10:45:37 +0000

    safe browser creation, and catching some errors

--- src/main.js
+++ src/main.js
@@ -7,23 +7,42 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const delayExecution = (delay_ms) => new Promise((resolve) => setTimeout(resolve, delay_ms));
-
+const PORT = 3100;
 const viewport = {
   width: 1920,
   height: 1080
 };
-let browser = null;
+
+let browsers = null;
+
+// TBD: this should block for anybody and return to anybody the same thing
+// currently parallel requests could overwrite browsers
+
+const launchBrowser = async function() {
+  console.debug("Launching new browser");
+  var b = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox', '--shm-size=1gb'],
+    executablePath: '/usr/bin/chromium-browser'
+  });
+  console.debug("Browser launched")
+  return b;
+}
 
 app.get('/health', (req,res) => {
-   return res.json({"success": true})
+  return res.json({success: true})
 })
 
 app.post('/screenshot', async (req,res) => {
   const { url, delay } = req.body;
-  console.log("getting from:", req.body, url, delay);
+  console.log("getting from:", req.body, url, delay || "no delay");
+  var page;
 
   try {
-    const page = await browser.newPage();
+    if (!browser) {
+      browser=await launchBrowser();
+    }
+    console.debug("Creating new page");
+    page = await browser.newPage();
     await page.setViewport(viewport);
     if (!delay) {
       await page.goto(url, { waitUntil: 'networkidle0' });
@@ -32,25 +51,32 @@ app.post('/screenshot', async (req,res) => {
       await delayExecution(delay);
     }
     const screenshot = await page.screenshot({ encoding: "binary" });
-    page.close();
-
-    console.log(screenshot);
-
     res.contentType("png");
-    return res.end(screenshot, 'binary');
+    res.end(screenshot, 'binary');
   } catch (error) {
-    console.log(error);
-    return res.json({error: true, name: error.name});
+    console.error("Error:", error);
+    res.json({error: true, name: error.name});
+  } finally {
+    if (page) {
+      console.debug("Closing Page");
+      page.close()
+    } else { //no page? that's strang! browser might be broken
+      console.debug("Closing Browser");
+      if (browser) browser.close()
+      browser=null //force relaunch
+    }
   }
+    return
 });
 
-(async () => {
-  browser = await puppeteer.launch({
-    args: ['--no-sandbox', '--disable-setuid-sandbox', '--shm-size=1gb'],
-    executablePath: '/usr/bin/chromium-browser'
-  });
+app.listen(PORT, function() {
+  console.log(`Started on PORT ${PORT}`);
+});
 
-  const PORT = 3100;
-  app.listen(PORT, () => console.log(`Started on PORT ${PORT}`));
 
-})();
+process.on('SIGUSR2', () => {
+  console.debug('SIGTERM signal received.');
+  if (browser) {
+    browser.close()
+  }
+});
